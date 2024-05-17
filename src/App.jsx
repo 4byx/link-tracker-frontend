0f3edd434303a8447a6f3992deebf2126351bafb
abhimanyu ahuja <abhimanyu.ahuja@rooter.io>
2024-05-17 13:44:39 +0530

    removed heading

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import Dashboard from './components/Dashboard';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>Link Tracker Dashboard</h1>
-            </header>
             <main>
                 <Dashboard />
             </main>
